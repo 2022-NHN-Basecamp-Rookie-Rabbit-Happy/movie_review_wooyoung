50ec5080e24840aae4aedb9ffb6fe0cf0ae76991
wooyoung <souljit2@gmail.com>
2022-01-13 14:09:38 +0900

    chore: 프로젝트 초기화 및 환경 설정

---
+++ src/main/java/com/example/movie_review_wooyoung/MovieReviewWooyoungApplication.java
@@ -0,0 +1,13 @@
+package com.example.movie_review_wooyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewWooyoungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewWooyoungApplication.class, args);
+	}
+
+}

7cade09bdc48696d0f9cec5cd394a6367ca4177b
wooyoung <souljit2@gmail.com>
2022-01-14 11:08:53 +0900

    feat: MovieDTO에 평점, 리뷰 갯수, 생성/수정 시간 데이터 추가

--- src/main/java/com/example/movie_review_wooyoung/dto/MovieDTO.java
+++ src/main/java/com/example/movie_review_wooyoung/dto/MovieDTO.java
@@ -1,11 +1,13 @@
 package com.example.movie_review_wooyoung.dto;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @Builder
@@ -19,4 +21,12 @@ public class MovieDTO {
 
     @Builder.Default
     private List<MovieImageDTO> imageDTOList = new ArrayList<>();
+
+    private double avg;
+
+    private int reviewCnt;
+
+    @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+    private LocalDateTime regDate, modDate;
+
 }

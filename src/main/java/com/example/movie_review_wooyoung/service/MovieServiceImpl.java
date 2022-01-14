9ca0622b449c08ad0e741ba95673391dcc61ae15
wooyoung <souljit2@gmail.com>
2022-01-14 11:16:56 +0900

    feat: MovieServiceImpl에 영화 목록 조회 기능 추가

--- src/main/java/com/example/movie_review_wooyoung/service/MovieServiceImpl.java
+++ src/main/java/com/example/movie_review_wooyoung/service/MovieServiceImpl.java
@@ -1,15 +1,22 @@
 package com.example.movie_review_wooyoung.service;
 
 import com.example.movie_review_wooyoung.dto.MovieDTO;
+import com.example.movie_review_wooyoung.dto.PageRequestDTO;
+import com.example.movie_review_wooyoung.dto.PageResultDTO;
 import com.example.movie_review_wooyoung.entity.Movie;
 import com.example.movie_review_wooyoung.entity.MovieImage;
 import com.example.movie_review_wooyoung.repository.MovieImageRepository;
 import com.example.movie_review_wooyoung.repository.MovieRepository;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -36,5 +43,22 @@ public class MovieServiceImpl implements MovieService {
         });
 
         return movie.getMno();
+    }
+
+    @Override
+    public PageResultDTO<MovieDTO, Object[]> getList(PageRequestDTO pageRequestDTO) {
+
+        Pageable pageable = pageRequestDTO.getPageable(Sort.by("mno").descending());
+
+        Page<Object[]> result = movieRepository.getListPage(pageable);
+
+        Function<Object[], MovieDTO> fn = arr -> entitiesToDTO(
+            (Movie) arr[0],
+            (List<MovieImage>) Arrays.asList((MovieImage) arr[1]),
+            (Double) arr[2],
+            (Long) arr[3]
+        );
+
+        return new PageResultDTO<>(result, fn);
     }
 }
